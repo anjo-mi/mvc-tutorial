6459cb3b2275e2a26b19949c6306dad911ff7bd4
kyle <nojaim.k@gmail.com>
2025-04-26 17:15:48 -0400

    step navigator up, need displays

--- js/tutorial.js
+++ js/tutorial.js
@@ -4,48 +4,105 @@ const directions = document.getElementById('directions');
 const serverImg = document.getElementById('serverImg');
 
 class Step{
-    constructor(narrator, codeImg, directions, serverImg){
+    constructor(narrator, codeImg, serverImg , otherProps = {}){
         this.narrator = narrator
         this.codeImg = codeImg
-        this.directions = directions
         this.serverImg = serverImg
+        this.subSteps = [];
+        this.next = null;
+        this.prev = null;
+
+        Object.assign(this, otherProps);
+    }
+
+    addSubStep(subStep){
+        this.subSteps.push(subStep);
+        return this;
     }
 }
 
-const steps = [];
-
-const displayStep = step => {
-    narrator.innerText = step.narrator;
-    codeImg.src = step.codeImg;
-    directions.innerText = step.directions;
-    serverImg.src = step.serverImg;
-};
-
-const displayNext = i => {
-    if (i >= steps.length - 1){
-        alert('its over, youve done it, close your ticket and tell the bobs to hop off it')
-        return;
+class List{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.currentStep = null;
+        this.currentSubStepIndex = -1;
     }
-    const text = directions.textContent;
-    let currIndex = steps.findIndex(step => step.directions = text);
-    console.log(currIndex)
-    displayStep(steps[currIndex + 1])
-}
 
-const displayPrev = i => {
-    if (i <= 0){
-        alert('theres no turning back, its mvc or bust')
-        return;
+    appendStep(step){
+        if (!this.head){
+            this.head = step;
+            this.tail = step;
+            this.currentStep = step;
+        }else{
+            this.tail.next = step;
+            step.prev = this.tail;
+            this.tail = step;
+        }
+        return step;
     }
-    const text = directions.textContent;
-    let currIndex = steps.findIndex(step => step.directions = text);
-    console.log(currIndex)
-    displayStep(steps[currIndex - 1])
+
+    next(){
+        if (!this.currentStep) return null;
+
+        if (this.currentStep.subSteps.length > 0 && 
+            this.currentSubStepIndex < this.currentStep.subSteps.length - 1){
+                this.currentSubStepIndex++;
+                this.displayCurrentContent();
+        }else if(this.currentStep.next){
+            this.currentStep = this.currentStep.next;
+            this.currentSubStepIndex = -1;
+            this.displayCurrentContent();
+        }
+        // to do add display animation!!!
+        return null;
+    }
+
+    prev(){
+        if (!this.currentStep) return null;
+
+        if (this.currentSubStepIndex >= 0){
+            this.currentSubStepIndex--;
+            this.displayCurrentContent();
+        }else if(this.currentStep.prev){
+            this.currentStep = this.currentStep.prev;
+            if (this.currentStep.subSteps.length > 0){
+                this.currentSubStepIndex = this.currentStep.subSteps.length - 1;
+            }else{
+                this.currentSubStepIndex = -1;
+            }
+            this.displayCurrentContent();
+        }
+        return null;
+    }
+
+    getCurrentContent(){
+        if (!this.currentStep) return null;
+
+        if (this.currentSubStepIndex >= 0 &&
+            this.currentSubStepIndex < this.currentStep.subSteps.length){
+                return {
+                    type: 'substep',
+                    content: this.currentStep.subSteps[this.currentSubStepIndex],
+                    mainStep: this.currentStep,
+                    subStepIndex: this.currentSubStepIndex,
+                };
+        }else{
+            return {
+                type: 'mainstep',
+                content: this.currentStep,
+                subStepIndex: -1,
+            };
+        }
+    }
+
+    displayCurrentContent(){
+        const stepData = this.getCurrentContent();
+    }
+
+    reset(){
+        this.currentStep = this.head;
+        this.currentSubStepIndex = -1;
+        this.displayCurrentContent();
+    }
 }
-
-document.getElementById('nextBtn').addEventListener('click', displayNext)
-document.getElementById('prevBtn').addEventListener('click', displayPrev)
-
-document.addEventListener('DOMContentLoaded', () => {
-    displayStep(steps[0])
-})
