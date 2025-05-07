1333d43d4453673112a1084870a4129c6696fe45
kyle <nojaim.k@gmail.com>
2025-05-07 18:07:23 -0400

    fixed hidden class specificity

--- js/tutorial.js
+++ js/tutorial.js
@@ -15,8 +15,8 @@ const serverImg = document.getElementById('serverImg');
 
 // next and prev buttons
 const mainPrev = document.getElementById('mainPrevBtn');
-const mainNext = document.getElementById('mainNextBtn');
 const subPrev = document.getElementById('subPrevBtn');
+const mainNext = document.getElementById('mainNextBtn');
 const subNext = document.getElementById('subNextBtn');
 
 class Step{
@@ -135,10 +135,16 @@ class List{
                 serverImg.src = mainStep.serverImg;
             }
             subStepText.textContent = content.innerMonologue;
-            if (content.changesImg){
+            if (!content.changesImg){
+                subStepImg.classList.add('hidden');
+            }else{
+                subStepImg.classList.remove('hidden');
                 subStepImg.src = content.changesImg;
             }
-            if (content.icon){
+            if (!content.icon){
+                subStepIcon.classList.add('hidden');
+            }else{
+                subStepIcon.classList.remove('hidden');
                 subStepIcon.textContent = content.icon;
             }
         }else if(type === 'mainstep'){
@@ -166,47 +172,36 @@ const step1 = new Step(
 const step1a = new SubStep(
     `didnt he just say nice job?? and why in the hell would i have thought to do that? and what the hell is mvc?`,
     null,
-    `&#129318;`
+    String.fromCodePoint(0x1F621)
 );
 const step1b = new SubStep(
     `whatever, i guess ive gota figure this out.. it seems like its basically the same thing, just organized a little differently`,
     null,
-    `&1F926;`
+    String.fromCodePoint(0x1F926)
+);
+const step1c = new SubStep(
+    `ok, so lets start with the M part of mvc.. it seems like i need to have a model with which to follow along when submitting data to the database`,
+    null,
+    String.fromCodePoint(0x1F9E0)
+);
+const step1d = new SubStep(
+    `i should prolly toss that in a models folder, and label the file with what it is.. so ill mkdir models and then touch models/Todo.js, and place my mongoose schema in there`,
+    `images/server-schema.png`,
 );
 step1.addSubStep(step1a);
 step1.addSubStep(step1b);
+step1.addSubStep(step1c);
+step1.addSubStep(step1d);
 list.appendStep(step1);
-const moveOver = list.next.bind(list);
-const moveBack = list.prev.bind(list);
-[mainNext,subNext].forEach(btn => btn.addEventListener('click', moveOver))
-// [mainPrev,subPrev].forEach(btn => btn.addEventListener('click', moveBack))
-mainPrev.addEventListener('click', moveBack)
-subPrev.addEventListener('click', moveBack)
-// console.log({mainNext,subNext,mainPrev,subPrev})
 list.displayCurrentContent();
-// class Step{
-//     constructor(narrator, fixInstructions, serverImg, bossImg , otherProps = {}){
-//         this.narrator = narrator;
-//         this.fixInstructions = fixInstructions;
-//         this.serverImg = serverImg;
-//         this.bossImg = bossImg;
-//         this.subSteps = [];
-//         this.next = null;
-//         this.prev = null;
 
-//         Object.assign(this, otherProps);
-//     }
+        
+document.addEventListener('DOMContentLoaded', () => {
+    const moveOver = list.next.bind(list);
+    const moveBack = list.prev.bind(list);
 
-//     addSubStep(subStep){
-//         this.subSteps.push(subStep);
-//         return this;
-//     }
-// }
-
-// class subStep{
-//     constructor(innerMonologue, changesImg = null, icon = null){
-//         this.innerMonologue = innerMonologue;
-//         this.changesImg = changesImg;
-//         this.icon = icon;
-//     }
-// }
+    mainNext.addEventListener('click', moveOver);
+    subNext.addEventListener('click', moveOver);
+    mainPrev.addEventListener('click', moveBack);
+    subPrev.addEventListener('click', moveBack);
+});
