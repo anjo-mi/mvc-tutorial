612c2dd5a05893c4383fa729058640d438314ae5
kyle <nojaim.k@gmail.com>
2025-05-13 21:03:19 -0400

    can update some styles but overall, i think were good

--- js/tutorial.js
+++ js/tutorial.js
@@ -190,6 +190,9 @@ class List{
             if (bossImg.src !== mainStep.bossImg){
                 bossImg.src = mainStep.bossImg;
             }
+            if (narrator.textContent !== mainStep.narrator){
+                narrator.textContent = mainStep.narrator;
+            }
             if (directions.textContent !== mainStep.fixInstructions){
                 await this.typeText(directions, mainStep.fixInstructions);
             }
@@ -198,7 +201,7 @@ class List{
                 setTimeout(() => {
                     subStepImg.style.display = 'none';
                 },400)
-            }else{
+            }else if(content.changesImg !== subStepImg.src){
                 subStepImg.src = content.changesImg;
                 subStepImg.style.display = 'block';
                 void subStepImg.offsetWidth;
@@ -273,48 +276,72 @@ const list = new List();
 // STEP 1 and substeps
 const step1 = new Step(
     'Project Manager',
-    `Nice job! You've just gotten our server running exactly the way we asked you to!.......... but..... you should have known that I wanted it done differently. You need to make it fit into industry best practices, and convert this whole thing to the MVC format.`,
+    `Nice job! You've just gotten our server running exactly the way we asked you to!.......... but..... obviously you should have known that I wanted it done differently! You need to make it fit into industry best practices, and convert this whole thing to the MVC format.`,
     'images/full-server.png',
     'images/bobs.jpeg'
 );
 const step1a = new SubStep(
-    `didnt he just say nice job?? and why in the hell would i have thought to do that? and what the hell is mvc?`,
+    `Didn't he just say nice job?? And why in the hell would I have thought to do that? And what the hell is MVC?`,
     null,
     String.fromCodePoint(0x1F621) //angry face
 );
 const step1b = new SubStep(
-    `whatever, i guess ive gota figure this out.. it seems like its basically the same thing, just organized a little differently`,
+    `Whatever, I guess I've gota figure this out.. it seems like its basically the same thing, just organized a little bit differently`,
     null,
     String.fromCodePoint(0x1F926) //face palm
 );
 const step1c = new SubStep(
-    `ok, so lets start with the M part of mvc.. it seems like i need to have a model with which to follow along when submitting data to the database`,
+    `OK, so let's start with the M part of MVC.. it seems like I need to have a model with which to follow along when submitting data to the database`,
     null,
     String.fromCodePoint(0x1F9E0) //brain
 );
 const step1d = new SubStep(
-    `i should prolly toss that in a models folder, and label the file with what it is.. so ill mkdir models and then touch models/Todo.js, and place my mongoose schema in there`,
+    `I should prolly toss that in a models folder, and label the file with what it is.. so I'll run "mkdir models" and then "touch models/Todo.js", and place my mongoose schema in there.`,
+    `images/server-schema.png`,
+);
+const step1d1 = new SubStep(
+    `For those of us who haven't experienced the joy that is Tears of the Kingdom, a Schema is essentially a blueprint. In this case it's going to dictate the format for data being inserted to the database.`,
+    `images/server-schema.png`,
+);
+const step1d2 = new SubStep(
+    `The Schema expects an object to be created with properties matching what will be accepted. In this instance, only objects that have properties 'todo' and 'completed' with types of 'String' and 'Boolean' respectively, will be accepted.`,
     `images/server-schema.png`,
 );
 const step1e = new SubStep(
-    `except.. goddamnit.. now i need to convert the implementation so it can be exported from the models/Todo.js file.. and make sure to require mongoose here as well`,
+    `Well that was easy... except.. goddamnit.. Now I need to convert the implementation so it can be exported from the models/Todo.js file.. and make sure to require mongoose here as well.`,
+    `images/models-schema.png`,
+);
+const step1e1 = new SubStep(
+    `Now that it's in a new file, I need to make certain that this file has access to mongoose by requiring it at the top (line 1).`,
+    `images/models-schema.png`,
+);
+const step1e2 = new SubStep(
+    `And since I'll be using it in a different file, I'll make sure to export it from here (line 14).`,
     `images/models-schema.png`,
 );
 const step1f = new SubStep(
-    `alright cool, that was easy enough.. and since i know that ill eventually be moving all of my get, post, put and delete methods out of the server, i wont waste the time importing the schema to the server`,
+    `Alright cool, that was easy enough.. and since I know that I'll eventually be moving all of my get, post, put and delete methods out of the server, I won't waste the time importing the schema to the server.`,
     null,
     String.fromCodePoint(0x1F9E0)
 );
 const step1g = new SubStep(
-    `but first, since ive taken the logic affecting database insertions out, i should prolly take out the logic involving the database connection, too`,
+    `But first, since I've taken the logic affecting database insertions out, I should prolly take out the logic involving the database connection, too.`,
     `images/server-connect.png`,
 );
 const step1h = new SubStep(
-    `and since its part of the configuration, ill move that to the config folder, and make a database.js file in which to put it.. then be certain to export it so i can still connect on the server (line 20 in server.js)`,
+    `Since it's part of the configuration, I'll move that to the config folder, and make a database.js file in which to put it.. then be certain to export it so I can still connect on the server (line 20 in server.js)`,
+    `images/config-connect.png`,
+);
+const step1h1 = new SubStep(
+    `I'll be certain to do the same thing here (require mongoose at the top [line 1]) so that I can use the mongoose syntax.`,
+    `images/config-connect.png`,
+);
+const step1h2 = new SubStep(
+    `And again, I'll make sure to export the const variable that I created, so that it can communicate with the server (line 18).`,
     `images/config-connect.png`,
 );
 const step1i = new SubStep(
-    `lastly, now i need to make sure my server.js file imports the configuration in order to connect to the database, since everything is still technically taking place on the server and i should be done with this step`,
+    `Lastly, now I need to make sure my server.js file imports the configuration in order to connect to the database, since everything is still technically taking place on the server. Then, I should be done with this step!`,
     `images/new-server-connect.png`,
     String.fromCodePoint(0x1F38A)//confetti
 );
@@ -322,100 +349,141 @@ const step1i = new SubStep(
 // STEP 2 and substeps
 const step2 = new Step(
     'Coworkers',
-    `Well would ya look at that! The new version of your server (hint: look below) looks pretty good..... BUT, we're all super annoying, and even though you've extracted all of the logic regarding connecting to the database, we'll certainly find a way to screw up your server methods`,
+    `Well would you look at that! The new version of your server (hint: look below) looks pretty darn good..... BUT, we're all super annoying, and even though you've extracted all of the logic regarding database connections, we'll certainly find a way to screw up your server methods! MUAHAHAHAHA!!!!`,
     'images/post-mod-server.png',
     'images/coworkers.jpg'
 );
 const step2a = new SubStep(
-    `just.... why?`,
+    `Just.... why?`,
     null,
     String.fromCodePoint(0x1F621) //angry face
 );
 const step2b = new SubStep(
-    `well at least i remembered this video from traversy media (see link at end!) about putting html files into a public folder and decided it was the same concept for ejs and views`,
-    null,
+    `Thankfully, I've already set up the server to render the pages from another folder's files, so they can't mess that up.`,
+    'images/server-views.png',
     String.fromCodePoint(0x1F64C) //celebration hands
 );
+const step2b1 = new SubStep(
+    `And all of my ejs files were already in the views folder, waiting to be accessed by the server!`,
+    'images/views-folder.png'
+);
 const step2c = new SubStep(
-    `so with the V of mvc handled (already had my ejs in a views folder), i guess the next logical step is to tackle the C`,
+    `So with the V of MVC handled, I guess the next logical step is to tackle the C.`,
     null,
     String.fromCodePoint(0x1F9E0)
 );
 const step2d = new SubStep(
-    `this is what 'controls' the behavior of my application, so i prolly need to make a folder for that, and file for any page that has control over the application`,
+    `This is what 'controls' the behavior of my application, so I'll prolly need to make another folder for that, and a file for any page that has control over the application.`,
     'images/controller-folder.png',
 );
 const step2e = new SubStep(
-    `well, lets eat the biggest frog first.. ill take all of my logic regarding the todo page out first`,
+    `Well, let's eat the biggest frog first.. I'll take all of my logic regarding the todo page out first.`,
+    'images/server-todo-methods.png',
+);
+const step2e1 = new SubStep(
+    `So any of the CRUD methods in my server that start with the path '/todos/', I'll extract.`,
     'images/server-todo-methods.png',
 );
 const step2f = new SubStep(
-    `and ill move them to the controllers/todos.js file, while making sure theyre in a format that can be exported to be used elsewhere`,
+    `And I'll move them to the controllers/todos.js file, while making sure theyre in a format that can be exported to be used elsewhere.`,
+    'images/contro-todo-methods.png',
+);
+const step2f1 = new SubStep(
+    `It seems really messy to export each method separately, though, so I'll big brain this bitch and reformat them all to be methods within an object.`,
+    'images/contro-todo-methods.png',
+);
+const step2f2 = new SubStep(
+    `Then I can just export the object as a whole using 'module.exports.'`,
     'images/contro-todo-methods.png',
 );
 const step2g = new SubStep(
-    `and CRUCIALLY! ill import my Todo variable from its new place of residence in the models folder, since the todo page needs to be able to talk to the database`,
+    `And !!!CRUCIALLY!!! I'll import my Todo variable from its new place of residence in the models folder, since the todo page needs to be able to talk to the database.`,
     'images/contro-import.png',
 );
 const step2h = new SubStep(
-    `and now ill eat the little frog.. because the home page should be easier since its only one method that does need to connect to the database`,
+    `Now I'll eat the little frog.. because the home page should be easier since it's only one method that does need to connect to the database.`,
     'images/server-home-method.png',
 );
 const step2i = new SubStep(
-    `and again ill convert it to a form that can be exported to still be used in the server, while not actually residing there`,
+    `And again I'll convert it to a form that can be exported to be used in the server, while not actually residing there.`,
+    'images/contro-home-method.png',
+);
+const step2i1 = new SubStep(
+    `Conveniently, this one does not need to communicate with the database, so there's no need to import anything from the models folder.`,
     'images/contro-home-method.png',
 );
 
 // STEP 3 and substeps
 const step3 = new Step(
     'Clueless Boss',
-    `hey, so im gona need you to stay late today, because git blamed you.. i tried submitting our TPS reports, but the server you built (hint: see below) doesnt have any sort of route to connect with the controllers`,
+    `Hey, so I'm going to need you to stay late today, because git blamed you.. I tried submitting our TPS reports, but the server you built (hint: see below) doesn't have any sort of route to connect with the controllers.`,
     'images/post-contro-server.png',
     'images/lumbergh.jpg'
 );
 const step3a = new SubStep(
-    `i hate him.. i hate him so goddamn much.. this was all working a couple hours ago and i swear they told me nice job.. but goddamnit hes right, i need to make a routes folder`,
+    `*rubs bridge of nose* I hate him.. I hate him so goddamn much.. this was all working a couple hours ago and I swear they told me nice job.. but goddamnit hes right, I need to make a routes folder.`,
     null,
     String.fromCodePoint(0x1F926)
 )
 const step3b = new SubStep(
-    `orrrrr maybeeeee.. ill just burn it all!`,
+    `orrrrr maybeeeee.. I'll just burn it all!`,
     null,
     String.fromCodePoint(0x1F525) + String.fromCodePoint(0x1F92C) // fire + angry
 )
 const step3c = new SubStep(
-    `*SIGH* no ill just make the folder. there are only two files so it shouldnt be too difficult..`,
+    `*SIGH* no I'll just make the folder. There are only two files so it shouldn't be too difficult..`,
     'images/routes-folder.png',
 )
 const step3d = new SubStep(
-    `ok, so these files are going to import from our controllers folder. and i need to make sure im picking from within the correct file in that folder.. so lets start with the todos again, and set up an express router`,
+    `OK, so these files are going to import from our controllers folder. And I need to make sure I'm picking from within the correct file in that folder.. so let's start with the todos again, and set up an express router.`,
+    'images/router-todos-imports.png',
+)
+const step3d1 = new SubStep(
+    `And I'll make sure that I'm importing the object that has all of my CRUD methods for the Todo page (line 3).`,
     'images/router-todos-imports.png',
 )
 const step3e = new SubStep(
-    `and now since all of my methods are defined in the controllers folder, all i have to do is set up 'listeners' to call them and export them so they can be used by the server`,
+    `Now, since all of my methods are defined in the controllers folder, all I have to do is set up 'listeners' to call them and export them so they can be used by the server.`,
+    'images/routes-todo.png',
+)
+const step3e1 = new SubStep(
+    `I should take note of the fact that, since this is now separated from the CRUD methods affecting other pages, I can omit the '/todos/' at the beginning of each path. *!!!and I should really remember this for a few steps from now!!!*`,
     'images/routes-todo.png',
 )
 const step3f = new SubStep(
-    `then i can do the same thing for the home page (make an express router, import the defined methods from the controllers folder, add them to the router, and export the router so the server can use it)`,
+    `Then I can do the same thing for the home page (make an express router, import the defined methods from the controllers folder, add them to the router, and export the router so the server can use it).`,
     'images/routes-home.png',
 )
 const step3g = new SubStep(
-    `so it seems like the last thing i need to do is import the routes to the server`,
+    `So, it seems like the last thing I need to do is import the routes to the server.`,
     'images/server-route-imports.png',
 )
 const step3h = new SubStep(
-    `and set up my app to use them!`,
+    `And set up my app to use them!`,
     'images/server-routes-use.png',
+)
+const step3h1 = new SubStep(
+    `Doing this is what allows the omission of the beginning of the paths in my routes folder.`,
+    'images/server-routes-use.png',
+)
+const step3h2 = new SubStep(
+    `I'm telling the server that for any requests made that start with '/' or '/todos/, to use the router file specified by the variable.`,
+    'images/server-routes-use.png',
+)
+const step3i = new SubStep(
+    `So that should be it! It's time to push the code and call it a day!`,
+    null,
+    String.fromCodePoint(0x1F38A) + String.fromCodePoint(0x1F64C)
 )
 // step 4 / conclusion
 const step4 = new Step(
     'Self Absorbed CEO',
-    `well well well.. would you look at what a great job I!!! did!`,
+    `Well well well.. would you look at what a great job I!!! did!`,
     'images/server-final.png',
     'images/ceo.jpg'
 );
 const step4a = new SubStep(
-    `thats it.. im out`,
+    `That's it.. I'm out`,
     'images/blow-up.png',
     String.fromCodePoint(0x1F926)
 )
@@ -424,23 +492,34 @@ step1.addSubStep(step1a);
 step1.addSubStep(step1b);
 step1.addSubStep(step1c);
 step1.addSubStep(step1d);
+step1.addSubStep(step1d1);
+step1.addSubStep(step1d2);
 step1.addSubStep(step1e);
+step1.addSubStep(step1e1);
+step1.addSubStep(step1e2);
 step1.addSubStep(step1f);
 step1.addSubStep(step1g);
 step1.addSubStep(step1h);
+step1.addSubStep(step1h1);
+step1.addSubStep(step1h2);
 step1.addSubStep(step1i);
 list.appendStep(step1);
 
 // add step 2 and its substeps
 step2.addSubStep(step2a);
 step2.addSubStep(step2b);
+step2.addSubStep(step2b1);
 step2.addSubStep(step2c);
 step2.addSubStep(step2d);
 step2.addSubStep(step2e);
+step2.addSubStep(step2e1);
 step2.addSubStep(step2f);
+step2.addSubStep(step2f1);
+step2.addSubStep(step2f2);
 step2.addSubStep(step2g);
 step2.addSubStep(step2h);
 step2.addSubStep(step2i);
+step2.addSubStep(step2i1);
 list.appendStep(step2);
 
 // add step 3 and its substeps
@@ -448,10 +527,15 @@ step3.addSubStep(step3a);
 step3.addSubStep(step3b);
 step3.addSubStep(step3c);
 step3.addSubStep(step3d);
+step3.addSubStep(step3d1);
 step3.addSubStep(step3e);
+step3.addSubStep(step3e1);
 step3.addSubStep(step3f);
 step3.addSubStep(step3g);
 step3.addSubStep(step3h);
+step3.addSubStep(step3h1);
+step3.addSubStep(step3h2);
+step3.addSubStep(step3i);
 list.appendStep(step3);
 
 // step 4 and substep / conclusion
